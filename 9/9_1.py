df6770c52606749a8e78f3aab421bce8537da486
Sebastian Stark <sstark@mailbox.org>
2023-12-09 21:28:00 +0100

    day 9 1

---
+++ 9/9_1.py
@@ -0,0 +1,27 @@
+#!/bin/python
+
+from sys import stdin
+from itertools import pairwise, starmap
+from functools import reduce
+from operator import add
+
+def sub(a, b):
+    return b - a
+
+def diffseq(arr):
+    return list(starmap(sub, pairwise(arr)))
+
+def nullseq(arr):
+    return reduce(add, arr) == 0
+
+res = 0
+for line in stdin:
+    seq = list(map(int, line.strip().split()))
+    res += seq[-1]
+    while True:
+        if nullseq(seq):
+            break
+        seq = diffseq(seq)
+        res += seq[-1]
+
+print(res)
