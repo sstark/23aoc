aff6b9a02d317fe8a341145c192f9bb9ef92c063
Sebastian Stark <sstark@mailbox.org>
2023-12-11 21:56:13 +0100

    generalize expand()

--- 11/11_1.py
+++ 11/11_1.py
@@ -6,22 +6,16 @@ from itertools import combinations, starmap
 def dist(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
-def expand(g):
+def expand(l, by=1):
     out = []
-    xvals = sorted([x[0] for x in g])
-    yvals = sorted([x[1] for x in g])
-    xgaps = set(range(xvals[-1])) - set(xvals)
-    ygaps = set(range(yvals[-1])) - set(yvals)
-    for galaxy in g:
-        x = galaxy[0]
-        y = galaxy[1]
-        for xgap in reversed(sorted(xgaps)):
-            if xgap < x:
-                x += 1
-        for ygap in reversed(sorted(ygaps)):
-            if ygap < y:
-                y += 1
-        out.append((x, y))
+    vals = sorted(l)
+    gaps = set(range(vals[-1])) - set(vals)
+    for e in l:
+        new = e
+        for gap in reversed(sorted(gaps)):
+            if gap < new:
+                new += by
+        out.append(new)
     return out
 
 galaxies = []
@@ -34,5 +28,8 @@ for line in stdin:
         x += 1
     y += 1
 
-galaxies = expand(galaxies)
+galaxies = zip(
+    expand([x[0] for x in galaxies]),
+    expand([x[1] for x in galaxies])
+)
 print(sum(starmap(dist, combinations(galaxies, 2))))
